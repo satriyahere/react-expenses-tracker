02d4f6873fd891385c439643ded6a643aac3fffa
satriyahere <90123775+satriyahere@users.noreply.github.com>
2021-09-07 11:44:30 +0700

    organized document

--- src/components/Expenses/ExpensesItem.js
+++ src/components/Expenses/ExpensesItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ExpensesItem.css';
 import ExpensesDate from './ExpensesDate';
-import Card from './Card';
+import Card from '../UI/Card';
 
 function ExpensesItem(props) {
   return (
--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ExpensesList.css';
 import ExpensesItem from './ExpensesItem';
-import Card from './Card';
+import Card from '../UI/Card';
 
 function ExpensesList(props) {
   return (
