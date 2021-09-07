ed8bd2525ff767c6b99473c5d1784646f363c0a3
satriyahere <90123775+satriyahere@users.noreply.github.com>
2021-09-07 19:19:45 +0700

    implement new expense form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import ExpensesList from './components/Expenses/ExpensesList';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   const expenses = [
@@ -24,7 +25,7 @@ const App = () => {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <ExpensesList expenses={expenses}></ExpensesList>
     </div>
   );
