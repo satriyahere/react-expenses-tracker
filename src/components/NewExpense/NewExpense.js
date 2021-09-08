912de25429d9f9d138b1fb241e92a1f943431817
satriyahere <90123775+satriyahere@users.noreply.github.com>
2021-09-08 18:31:27 +0700

    implement hide / show form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,27 @@
 import { nanoid } from 'nanoid';
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+  const [isShowForm, setIsShowForm] = useState(false);
   const addExpenseHandler = (formExpenseData) => {
     const expenseData = { ...formExpenseData, id: nanoid() };
     props.onAddExpense(expenseData);
+    setIsShowForm(false);
   };
-  return (
-    <div className='new-expense'>
-      <ExpenseForm onAddExpense={addExpenseHandler} />
-    </div>
+  const showFormHandler = () => {
+    setIsShowForm(true);
+  };
+  const hideFormHandler = () => {
+    setIsShowForm(false);
+  };
+  const addExpenseContent = isShowForm ? (
+    <ExpenseForm onCancel={hideFormHandler} onAddExpense={addExpenseHandler} />
+  ) : (
+    <button onClick={showFormHandler}>Add New Expenses</button>
   );
+  return <div className='new-expense'>{addExpenseContent}</div>;
 };
 
 export default NewExpense;
